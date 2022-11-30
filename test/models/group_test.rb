cdc09bd7d71063285e91c1bb98b0d01aaf78e6a9
Phạm Minh Quang <quangpham.245@protonmail.com>
2022-11-30 16:09:02 +0700

    migrate db and add test for group model

--- test/models/group_test.rb
+++ test/models/group_test.rb
@@ -1,7 +1,45 @@
-require "test_helper"
+require 'test_helper'
 
 class GroupTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should save group when name, kind, user_id is provided and user_id exists and kind is valid' do
+    user = User.new email: 'test@example.com', password_hash: 'abc'
+    assert user.save, 'Failed to setup - Failed to create user'
+
+    group = Group.new name: 'test', kind: 'income', user_id: user.id
+    assert group.save, 'Failed to create group'
+  end
+
+  test 'should not save group when name is missing' do
+    user = User.new email: 'test@example.com', password_hash: 'abc'
+    assert user.save, 'Failed to setup - Failed to create user'
+
+    group = Group.new kind: 'income', user_id: user.id
+    assert_not group.save, 'Group saved, even without name'
+  end
+
+  test 'should not save group when kind is missing' do
+    user = User.new email: 'test@example.com', password_hash: 'abc'
+    assert user.save, 'Failed to setup - Failed to create user'
+
+    group = Group.new name: 'test', user_id: user.id
+    assert_not group.save, 'Group saved, even without kind'
+  end
+
+  test 'should not save group when kind is not `income` or `expense`' do
+    user = User.new email: 'test@example.com', password_hash: 'abc'
+    assert user.save, 'Failed to setup - Failed to create user'
+
+    group = Group.new name: 'test', user_id: user.id, kind: 'not valid kind'
+    assert_not group.save, 'Group saved, even with invalid kind'
+  end
+
+  test 'should not save group when user_id is missing' do
+    group = Group.new name: 'test', kind: 'not valid kind'
+    assert_not group.save, 'Group saved, even without user_id'
+  end
+
+  test 'should not save group when user doesnt exist' do
+    group = Group.new name: 'test', kind: 'not valid kind', user_id: 999
+    assert_not group.save, 'Group saved, even without user_id'
+  end
 end

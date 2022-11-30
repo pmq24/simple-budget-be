82456810091bf3cc1b6213d7b13753768de1f859
Phạm Minh Quang <quangpham.245@protonmail.com>
2022-11-30 15:49:53 +0700

    generate group model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_28_165245) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_30_084920) do
+  create_table "groups", force: :cascade do |t|
+    t.string "name"
+    t.string "type"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_groups_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "password_hash"
@@ -18,4 +27,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_28_165245) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "groups", "users"
 end
